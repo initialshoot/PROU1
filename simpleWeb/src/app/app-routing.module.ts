87e9557b4063462b15084cb7018fe625921d34fb
Omar_asc <initialshoot@outlook.com>
2022-05-30 14:50:07 -0500

    add navbar and routes

--- simpleWeb/src/app/app-routing.module.ts
+++ simpleWeb/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './sections/home/home.component';
+import { AboutComponent } from './sections/about/about.component';
+import { PortfolioComponent } from './sections/portfolio/portfolio.component';
+import { PagesComponent } from './sections/pages/pages.component';
+import { BlogComponent } from './sections/blog/blog.component';
+import { ContactComponent } from './sections/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent },
+  { path: 'portfolio', component: PortfolioComponent},
+  { path: 'pages', component: PagesComponent},
+  { path: 'blog', component: BlogComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: '**', component: HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
